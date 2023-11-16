7df40a612fd1b3564f5d7d2785cedd456b4fd48b
ritaOmondi <cherylritaomondi@gmail.com>
2023-11-15 21:48:09 -0800

    fixing betty style

--- main.c
+++ main.c
@@ -9,40 +9,37 @@
  */
 int main(int argc, char *argv[])
 {
-    char *buffer = NULL;
-    char *str = NULL;
-    size_t len = 0;
-    unsigned int line_number = 1;
-    stack_t *stack = NULL;
-    FILE *file = NULL; 
+	char *buffer = NULL;
+	char *str = NULL;
+	size_t len = 0;
+	unsigned int line_number = 1;
+	stack_t *stack = NULL;
+	FILE *file = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
 file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    while (getline(&buffer, &len, file) != -1)
-    {
-        if (*buffer != '\n')
-        {
-		str = strtok(buffer, "\n");
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (getline(&buffer, &len, file) != -1)
+{
+if (*buffer != '\n')
+{
+str = strtok(buffer, "\n");
 pass_token(str, &stack, line_number);
-	}
-
-        line_number++;
-    }
-
-    fclose(file);
-    free(buffer);
-    if (stack != NULL)
-		free_stack(&stack, line_number);
-    return 0;
+}
+line_number++;
+}
+fclose(file);
+free(buffer);
+if (stack != NULL)
+free_stack(&stack, line_number);
+return (0);
 }
 
